7d68d3b981809c1fd17ce0db5bb278eb6ccd1080
Polinka <Polinka@DESKTOP-MHBJDTO>
2023-06-29 19:24:22 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число: ");
+int first = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int second = int.Parse(Console.ReadLine());
+
+int Sum(int first, int second)
+{
+    int s = first + second;
+    return s;
+}
+Console.WriteLine();
+int result = Sum(first, second);
+Console.WriteLine($"{first} + {second} = {result}");
